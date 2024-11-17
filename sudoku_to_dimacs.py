35f3f1a26f523242188933db118779e9a5179284
Puck Voorham <puckvoorham@gmail.com>
2024-11-17 11:20:41 +0100

    added symmetric sudoku generation

--- sudoku_to_dimacs.py
+++ sudoku_to_dimacs.py
@@ -58,9 +58,10 @@ def convert_sudokus_to_dimacs(sudoku_file_name, dir_path, file_names):
         if grid_size not in dimacs:
             print(f"Rules not found for grid size {grid_size}")
             continue
+
         puzzle_to_dimacs(line, grid_size, dimacs[grid_size], num)
 
-sudoku_file_name = "4x4.txt"
+sudoku_file_name = "1000 sudokus.txt"
 rule_files = ["sudoku-rules-4x4.txt", "sudoku-rules-9x9.txt", "sudoku-rules-16x16.txt"]
 rules_dir = "data/"
 convert_sudokus_to_dimacs(sudoku_file_name, rules_dir, rule_files)
